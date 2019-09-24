12ca88aeea1ef44c95e887d8c445b671b31403fa
naohashizume <naobridge46@gmail.com>
2019-09-24 14:40:11 -0700

    Updates

---
+++ precache-manifest.62396617cd0f58a85dd647b0553443be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18489a377db1913e3fa80ddce524f3ae",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "30b89cef10f7a16d60c4",
+    "url": "/react-to-do/static/css/main.f5a24e2c.chunk.css"
+  },
+  {
+    "revision": "e4874aafdc0a7bbac4a7",
+    "url": "/react-to-do/static/js/2.6cb50b36.chunk.js"
+  },
+  {
+    "revision": "30b89cef10f7a16d60c4",
+    "url": "/react-to-do/static/js/main.b7377bf1.chunk.js"
+  },
+  {
+    "revision": "907bbe0eddf9c3f4e171",
+    "url": "/react-to-do/static/js/runtime-main.f618c804.js"
+  }
+]);
